fe3a77e3f511049b4d10c600f069bd4c1e23b91d
Xavier John <xavierjohn@hotmail.com>
2022-10-27 12:02:06 -0700

    API Versioning Swagger Sample

---
+++ OpenApiExample/Netural/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿namespace AspVersioning.Examples.Netural.Controllers;
+
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// Is netural API should show up with all versions.
+/// </summary>
+[ApiVersionNeutral]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    /// <summary>
+    /// Retrieves all orders.
+    /// </summary>
+    /// <returns>All available orders.</returns>
+    /// <response code="200">Orders successfully retrieved.</response>
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(string), 200)]
+    public IActionResult Get()
+    {
+        return Ok("I am up and running");
+    }
+}
